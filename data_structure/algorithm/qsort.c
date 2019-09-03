8809eacf40a37026d55b3d80438a980919b138bb
lgh-007 <1445090122@qq.com>
2019-09-03 11:37:13 +0800

    2019-9-3

--- data_structure/algorithm/qsort.c
+++ data_structure/algorithm/qsort.c
@@ -71,19 +71,15 @@ int quicksort(int a[],int low,int high){
 }
 /*插入排序*/
 void InsertionSort(int *num,int n){
-  int i = 0;
-  int j = 0;
-  int tmp = 0;
-  for(i = 1;i<n;i++)
-  {
-    tmp = num[i];//从待插入组取出第一个元素。
-    j = i-1; //i-1即为有序组最后一个元素（与待插入元素相邻）的下标
-    while(j>=0&&tmp<num[j])  //注意判断条件为两个，j>=0对其进行边界限制。第二个为插入判断条件
-    {
-      num[j+1] = num[j];//若不是合适位置，有序组元素向后移动
+  int i,j,tmp;
+  for(i = 1;i<n;i++){
+    tmp = num[i];
+    j = i-1;
+    while (j >= 0 && tmp < num[j]) {
+      num[j+1] = num[j];//往后移动一次
       j--;
     }
-    num[j+1] = tmp;//找到合适位置，将元素插入。
+    num[j+1] = tmp;//将元素插入。
   }
 }
 /*系统自带库函数的使用*/
